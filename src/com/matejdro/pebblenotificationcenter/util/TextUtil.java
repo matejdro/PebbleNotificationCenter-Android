bd7c405d1ae4a72e6e56b2a9b7084366d6505108
Arieh Glazer <arieh.glazer@gmail.com>
2013-12-28 09:10:04 +0200

    reverting some minor formatting and local changes

--- src/com/matejdro/pebblenotificationcenter/util/TextUtil.java
+++ src/com/matejdro/pebblenotificationcenter/util/TextUtil.java
@@ -1,92 +1,87 @@
 package com.matejdro.pebblenotificationcenter.util;
 
-import java.io.Console;
-
-
-
 import java.util.HashMap;
 
-import com.matejdro.pebblenotificationcenter.util.RTLUtility;
 import com.matejdro.pebblenotificationcenter.PebbleNotificationCenter;
+import com.matejdro.pebblenotificationcenter.notifications.NotificationHandler;
 
 
-public class TextUtil {
-    public static String prepareString(String text)
-    {
-        return prepareString(text, 20);
-    }
+public class TextUtil {	
+	public static String prepareString(String text)
+	{
+		return prepareString(text, 20);
+	}
 
-    public static String prepareString(String text, int length)
-    {
-        text = fixInternationalAndTrim(text, length);
+	public static String prepareString(String text, int length)
+	{
+		text = fixInternationalAndTrim(text, length);
         
         if (RTLUtility.getInstance().isRTL(text)){            
             text = RTLUtility.getInstance().format(text, 15);        	
         }
         
-        return trimString(text, length, true);
-    }
+		return trimString(text, length, true);
+	}
 
-	
 
-    public static String fixInternationalAndTrim(String text, int length)
-    {
-        StringBuilder builder = new StringBuilder(length);
+	public static String fixInternationalAndTrim(String text, int length)
+	{
+		StringBuilder builder = new StringBuilder(length);
 
-        length = Math.min(length, text.length());
+		length = Math.min(length, text.length());
 
-        HashMap<Character, String> replacementTable = PebbleNotificationCenter.getInMemorySettings().getReplacingStrings();
+		HashMap<Character, String> replacementTable = PebbleNotificationCenter.getInMemorySettings().getReplacingStrings();
 
-        for (int i = 0; i < length; i++)
-        {
-            char ch = text.charAt(i);
+		for (int i = 0; i < length; i++)
+		{
+			char ch = text.charAt(i);
 
-            String replacement = replacementTable.get(ch);
-            if (replacement != null)
-            {
-                builder.append(replacement);
-            }
-            else
-            {
-                builder.append(ch);
-            }
-        }
+			String replacement = replacementTable.get(ch);
+			if (replacement != null)
+			{
+				builder.append(replacement);
+			}
+			else
+			{
+				builder.append(ch);
+			}
+		}
 
-        return builder.toString();
-    }
+		return builder.toString();		
+	}
 
-    public static String trimString(String text)
-    {
-        return trimString(text, 20, true);
-    }
+	public static String trimString(String text)
+	{
+		return trimString(text, 20, true);
+	}
 
-    public static String trimString(String text, int length, boolean trailingElipsis)
-    {
-        if (text == null)
-            return null;
+	public static String trimString(String text, int length, boolean trailingElipsis)
+	{
+		if (text == null)
+			return null;
 
-        int targetLength = length;
-        if (trailingElipsis)
-        {
-            targetLength -= 3;
-        }
+		int targetLength = length;
+		if (trailingElipsis)
+		{
+			targetLength -= 3;
+		}
 
-        if (text.getBytes().length > length)
-        {
-            if (text.length() > targetLength)
-                text = text.substring(0, targetLength);
+		if (text.getBytes().length > length)
+		{
+			if (text.length() > targetLength)
+				text = text.substring(0, targetLength);
 
-            while (text.getBytes().length > targetLength)
-            {
-                text = text.substring(0, text.length() - 1);
-            }
+			while (text.getBytes().length > targetLength)
+			{
+				text = text.substring(0, text.length() - 1);
+			}
 
-            if (trailingElipsis)
-                text = text + "...";
+			if (trailingElipsis)
+				text = text + "...";
 
-        }
+		}
 
-        return text;
+		return text;
 
-    }
+	}
 }
