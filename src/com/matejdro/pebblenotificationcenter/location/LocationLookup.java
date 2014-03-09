0f114a2cdd0e9409d868ca4ef702aee7c4dd80f5
matejdro <matejdro@gmail.com>
2014-03-09 16:11:03 +0100

    Fixed #14

--- src/com/matejdro/pebblenotificationcenter/location/LocationLookup.java
+++ src/com/matejdro/pebblenotificationcenter/location/LocationLookup.java
@@ -32,8 +32,7 @@ public class LocationLookup {
   }
 
   public void lookup() {
-    boolean backlight =
-        settings.getInt(PebbleNotificationCenter.LIGHT_SCREEN_ON_NOTIFICATIONS, 1)==3;
+    boolean backlight = Integer.parseInt(settings.getString(PebbleNotificationCenter.LIGHT_SCREEN_ON_NOTIFICATIONS, "2")) == 3;
     // will lookup time once a hour
     if (backlight && TimeUtil.hasTimePassed(gotLastLocation, 60 * 60 * 1000)) {
       // Acquire a reference to the system Location Manager
