05d011bfebeb5b1d0e435d897344cfcf317e5a80
Matej <matejdro@gmail.com>
2014-09-17 12:04:21 +0200

    Removing options that were moved to PerAppSettings

--- src/com/matejdro/pebblenotificationcenter/PebbleNotificationCenter.java
+++ src/com/matejdro/pebblenotificationcenter/PebbleNotificationCenter.java
@@ -8,15 +8,12 @@ import com.matejdro.pebblenotificationcenter.util.SettingsMemoryStorage;
 public class PebbleNotificationCenter extends android.app.Application {
     public static final String APP_INCLUSION_MODE = "includeMode";
     public static final String SELECTED_PACKAGES = "CheckedApps";
-    public static final String REGEX_INCLUSION_MODE = "regexMode";
-    public static final String REGEX_LIST = "BlacklistRegexes";
     public static final String REPLACING_KEYS_LIST = "ReplacingKeys";
     public static final String REPLACING_VALUES_LIST = "ReplacingValues";
     public static final String FONT_TITLE = "fontTitle";
     public static final String FONT_SUBTITLE = "fontSubtitle";
     public static final String FONT_BODY = "fontBody";
     public static final String LIGHT_SCREEN_ON_NOTIFICATIONS = "lightScreenNotifications";
-    public static final String VIBRATION_MODE = "vibrateMode";    
     public static final String CLOSE_TO_LAST_APP = "closeToLastApp";
     public static final String DONT_VIBRATE_WHEN_CHARGING = "noVibrateCharge";
     public static final String SHAKE_ACTION = "shakeAction";
