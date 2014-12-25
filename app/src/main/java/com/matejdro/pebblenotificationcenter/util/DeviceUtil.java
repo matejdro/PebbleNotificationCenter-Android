63255c0e0eda7d20da7298c97651a7d6a6e3e23d
Matej <matejdro@gmail.com>
2014-12-25 13:56:43 +0100

    Fixed crash on non-lollipop devices

--- app/src/main/java/com/matejdro/pebblenotificationcenter/util/DeviceUtil.java
+++ app/src/main/java/com/matejdro/pebblenotificationcenter/util/DeviceUtil.java
@@ -20,7 +20,7 @@ public class DeviceUtil {
     public static boolean isScreenOn(Context context) {
         PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT_WATCH)
             return pm.isInteractive();
         else
             //noinspection deprecation
