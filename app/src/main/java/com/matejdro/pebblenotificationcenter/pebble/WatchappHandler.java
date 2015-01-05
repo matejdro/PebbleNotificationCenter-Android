1cf2528f453725be71db0625759dc0e38966b529
Matej <matejdro@gmail.com>
2015-01-05 21:30:24 +0100

    Version and protocol change

--- app/src/main/java/com/matejdro/pebblenotificationcenter/pebble/WatchappHandler.java
+++ app/src/main/java/com/matejdro/pebblenotificationcenter/pebble/WatchappHandler.java
@@ -16,7 +16,7 @@ import com.matejdro.pebblenotificationcenter.R;
 
 public class WatchappHandler extends BroadcastReceiver
 {
-    public static final int SUPPORTED_PROTOCOL = 26;
+    public static final int SUPPORTED_PROTOCOL = 27;
     public static final String INTENT_UPDATE_WATCHAPP = "com.matejdro.pebblenotificationcenter.UPDATE_WATCHAPP";
 
     public static final String WATCHAPP_URL = "https://dl.dropboxusercontent.com/u/6999250/dialer/Center/notificationcenter.pbw";
